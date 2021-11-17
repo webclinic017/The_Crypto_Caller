7139cc5aa00fd9780891f6715bc0877dd1e5f422
Billy <billy.bishop@rice.edu>
2021-11-17 17:47:34 -0600

    Testing alpaca code

---
+++ workflow/billy_workflow/config.py
@@ -0,0 +1,10 @@
+API_KEY = 'AK2IIL02Q0ZDWFD95GDU'
+
+SECRET_KEY = '35RyAIJqm1eBhwViUDlVEklcbaWQULpg02mTTjZv'
+
+BARS_URL = 'https://data.alpaca.markets/v1beta1/crypto'
+
+HEADERS = {
+    'APCA-API-KEY-ID' : API_KEY,
+    'APCA-API-SECRET-KEY' : SECRET_KEY
+}
